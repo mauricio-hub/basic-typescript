b7260765f834575147b5a96231abafb4cd7ce8d8
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2024-10-19 17:25:48 -0500

    basic typescript

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { Todo } from "./components/Todo";
+
+interface DataTodo {
+  todos: {
+    completed: boolean;
+    id: number;
+    todo: string;
+    userId: number;
+  }[]; //aqui le decimos que es un array de objetos
+  total : number;
+  skip : number;
+  limit : number;
+}
+
+//React.FC significa que es una función de componente de React
+export const App: React.FC = () => {
+  //<string> es el tipo de dato que se espera
+  const [name, setName] = useState<string>("Mafalda");
+  //si se especifiaca el tipo de dato no es necesario poner el tipo de dato en el useState
+  //ya que se entiende que tipo de dato es
+  const [isActived, setIsActived] = useState(false);
+
+  // todolist es un array de objetos que tiene la estructura de DataTodo
+  // otro ejemplo igual useState<Array<DataTodo>>([])
+  const [todosList, setTodosList] = useState<DataTodo>();
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const data = await fetch("https://dummyjson.com/todos");
+      const result = await data.json();
+      setTodosList(result);
+    };
+
+    getTodos();
+  }, []);
+
+  return (
+    <div>
+      {todosList?.todos.map((todo) => {
+        return <Todo key={todo.id} todo={todo.todo} status={todo.completed ? 'active' : 'inactive'} />
+
+      })}
+    </div>
+  );
+};
---
+++ src/components/Todo.tsx
@@ -0,0 +1,35 @@
+
+import React from 'react';
+
+type TodoStatus = 'active' | 'inactive';
+
+interface TodoProps {
+    todo: string;
+    status?: TodoStatus;
+}
+
+export const Todo: React.FC<TodoProps> = ({ todo, status }: TodoProps) => {
+
+    const statusColor = status === 'active' ? '#4caf50' : '#f44336';
+
+    const handleClick = (evt:React.MouseEvent) => {
+        console.log('click', evt);
+    };
+
+    return (
+        <div className="todo-container">
+            <div className="todo-content">
+                <span className="todo-icon" role="img" aria-label="status">
+                    {status === 'active' ? '✅' : '🕒'}
+                </span>
+                <div className="todo-text">
+                    <i>{todo}</i>
+                    <h4 style={{ backgroundColor: statusColor, color: 'white', padding: '5px 10px', borderRadius: '5px' }}>
+                        {status === 'active' ? 'Active' : 'Inactive'}
+                    </h4>
+                </div>
+            </div>
+            <button onClick={handleClick}>text</button>
+        </div>
+    );
+};
